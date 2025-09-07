1ab3cd321cf1d3a0b99041c186af26863be39962
Andy Barlow <iamandybarlow@gmail.com>
2025-09-07 16:47:39 +0100

    remove migration from v2 namespace

--- iana-registry-data-lib/src/index.ts
+++ iana-registry-data-lib/src/index.ts
@@ -1,3 +1,5 @@
-export { OAuthRegistry } from "./registries/oauth_registry/index";
-export { JWTRegistry } from "./registries/jwt_registry/index";
-export { JOSERegistry } from "./registries/jose_registry/index";
+export type { V2RegistryDataset, V2RegistryEntry, V2RegistryMetadata } from './types.v2';
+
+export * as OAuth from './registries/oauth_registry/index';
+export * as JOSE from './registries/jose_registry/index';
+export * as JWT from './registries/jwt_registry/index';
--- iana-registry-data-lib/src/registries/jose_registry/index.ts
+++ iana-registry-data-lib/src/registries/jose_registry/index.ts
@@ -1,17 +1,17 @@
-import JsonWebEncryptionCompressionAlgorithms from './json_web_encryption_compression_algorithms.json';
-import JsonWebKeyEllipticCurve from './json_web_key_elliptic_curve.json';
-import JsonWebKeyOperations from './json_web_key_operations.json';
-import JsonWebKeyParameters from './json_web_key_parameters.json';
-import JsonWebKeySetParameters from './json_web_key_set_parameters.json';
-import JsonWebKeyTypes from './json_web_key_types.json';
-import JsonWebKeyUse from './json_web_key_use.json';
+import json_web_encryption_compression_algorithms from './json_web_encryption_compression_algorithms.json';
+import json_web_key_elliptic_curve from './json_web_key_elliptic_curve.json';
+import json_web_key_operations from './json_web_key_operations.json';
+import json_web_key_parameters from './json_web_key_parameters.json';
+import json_web_key_set_parameters from './json_web_key_set_parameters.json';
+import json_web_key_types from './json_web_key_types.json';
+import json_web_key_use from './json_web_key_use.json';
 
-export class JOSERegistry {
-  public static JsonWebEncryptionCompressionAlgorithms = JsonWebEncryptionCompressionAlgorithms;
-  public static JsonWebKeyEllipticCurve = JsonWebKeyEllipticCurve;
-  public static JsonWebKeyOperations = JsonWebKeyOperations;
-  public static JsonWebKeyParameters = JsonWebKeyParameters;
-  public static JsonWebKeySetParameters = JsonWebKeySetParameters;
-  public static JsonWebKeyTypes = JsonWebKeyTypes;
-  public static JsonWebKeyUse = JsonWebKeyUse;
-}
+export {
+  json_web_encryption_compression_algorithms,
+  json_web_key_elliptic_curve,
+  json_web_key_operations,
+  json_web_key_parameters,
+  json_web_key_set_parameters,
+  json_web_key_types,
+  json_web_key_use,
+};
