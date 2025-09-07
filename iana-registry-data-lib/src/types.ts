7935725d6f68ed415fa72fb33529d3cc0dcfff82
Andy Barlow <iamandybarlow@gmail.com>
2025-09-07 22:58:24 +0100

    a couple of missed migration files removed

--- iana-registry-data-lib/src/types.ts
+++ iana-registry-data-lib/src/types.ts
@@ -1,20 +1,19 @@
-export interface V2RegistryEntry {
+export interface RegistryEntry {
   entry_id: string;
   [key: string]: unknown;
 }
 
-export interface V2RegistryMetadata {
+export interface RegistryMetadata {
   datasource_url: string;
   required_specifications: string[];
   last_updated_iso: string;
 }
 
-export interface V2RegistryDataset {
+export interface RegistryDataset {
   schema_version: 2;
   registry_id: string;
   dataset_id: string;
   name: string;
-  metadata: V2RegistryMetadata;
-  entries: V2RegistryEntry[];
+  metadata: RegistryMetadata;
+  entries: RegistryEntry[];
 }
-
